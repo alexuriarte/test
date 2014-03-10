b72282f2c1c36ce2aba503e8c936cb8907cf1fa0
Thomas Orozco <thomas@orozco.fr>
2014-03-10 21:39:38 +0100

    Handle stress's weird handling of children

--- functions.php
+++ functions.php
@@ -5,21 +5,21 @@ function pid_from_file($pidfile) {
   return str_replace(array("\r", "\n"), '', @file_get_contents($pidfile));
 }
 
+function get_pids_for_group($parent_pid) {
+  // Get the children
+  $pids = explode("\n", shell_exec(sprintf("ps --ppid %s -o pid=", $parent_pid)));
+
+  // Add the parent
+  array_push($pids, shell_exec(sprintf("ps --pid %s -o pid=", $parent_pid)));
+
+  // Filter out empty strings!
+  return array_filter($pids, "strlen");
+}
+
 function is_running($pidfile) {
   $pid = pid_from_file($pidfile);
-
-  if ($pid === "") {
-    return false;
-  }
-
-  try {
-    // Very insecure, but we don't really care here.
-    $result = shell_exec(sprintf("ps %d", $pid));
-    if (count(preg_split("/\n/", $result)) > 2) {
-      return true;
-    }
-  } catch(Exception $e) {}
-  return false;
+  $pids = get_pids_for_group($pid);
+  return count($pids) > 0;
 }
 
 function start_process($cmd, $outputfile, $pidfile) {
@@ -27,8 +27,12 @@ function start_process($cmd, $outputfile, $pidfile) {
 }
 
 function kill_process($pidfile) {
-  $pid = pid_from_file($pidfile);
-  posix_kill($pid, SIGTERM);
+  $parent_pid = pid_from_file($pidfile);
+  $pids = get_pids_for_group($parent_pid);
+
+  foreach($pids as $pid) {
+    exec(sprintf("kill %s", $pid));
+  }
 }
 
 function get_n_cpus() {
